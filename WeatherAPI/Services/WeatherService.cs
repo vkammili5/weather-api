67f524826846c2e8195dcb2a81400d9df8e1fba8
iamfranco <kinfaifrancochan@gmail.com>
2022-07-01 15:51:22 +0100

    refactor WeatherService GetWeatherByLatLonAsync method

--- WeatherAPI/Services/WeatherService.cs
+++ WeatherAPI/Services/WeatherService.cs
@@ -19,41 +19,48 @@ public class WeatherService : IWeatherService
         string url = "https://api.open-meteo.com/v1/forecast?" +
             $"latitude={latitude}&" +
             $"longitude={longitude}&" +
-            "daily=weathercode,temperature_2m_max,temperature_2m_min&" +
+            "daily=weathercode&" +
             "timezone=Europe%2FLondon";
 
         HttpResponseMessage response = await _httpClient.GetAsync(url);
 
         string responseString = await response.Content.ReadAsStringAsync();
-
-        JObject responseObject = JObject.Parse(responseString);
-
-        var a = responseObject["daily"]["weathercode"].ToList();
-
-        List<WeatherCode> listOfWeatherCode = new();
-        foreach (var x in a)
-        {
-            int codeNumber = int.Parse(x.ToString());
-            WeatherCode weatherCode = codeNumber switch
-            {
-                0 => WeatherCode.ClearSky,
-                <= 3 => WeatherCode.Cloudy,
-                < 51 => WeatherCode.Fog,
-                _ => WeatherCode.RainOrWorse
-            };
-
-            listOfWeatherCode.Add(weatherCode);
-        }
-
-        Weather weather = new Weather()
-        {
-            latitude = double.Parse(responseObject["latitude"].ToString()),
-            longitude = double.Parse(responseObject["longitude"].ToString()),
-            startDate = DateTime.Parse(responseObject["daily"]["time"].First().ToString()),
-            endDate = DateTime.Parse(responseObject["daily"]["time"].Last().ToString()),
-            weatherCodes = listOfWeatherCode
-        };
+        Weather weather = ParseJsonStringToWeather(responseString);
 
         return weather;
+    }
+
+    private static Weather ParseJsonStringToWeather(string responseString)
+    {
+        JObject responseObject = JObject.Parse(responseString);
+
+        var weatherCodeJTokenList = responseObject["daily"]!["weathercode"]!.ToList();
+
+        List<WeatherCode> weatherCodeList = weatherCodeJTokenList.Select(x =>
+        {
+            int weatherCodeNumber = int.Parse(x.ToString());
+            return NumberToWeatherCode(weatherCodeNumber);
+        }).ToList();
+
+        Weather weather = new ()
+        {
+            latitude = double.Parse(responseObject["latitude"]!.ToString()),
+            longitude = double.Parse(responseObject["longitude"]!.ToString()),
+            startDate = DateTime.Parse(responseObject["daily"]!["time"]!.First().ToString()),
+            endDate = DateTime.Parse(responseObject["daily"]!["time"]!.Last().ToString()),
+            weatherCodes = weatherCodeList
+        };
+        return weather;
+    }
+
+    private static WeatherCode NumberToWeatherCode(int weatherCodeNumber)
+    {
+        return weatherCodeNumber switch
+        {
+            <= 1 => WeatherCode.ClearSky,
+            <= 3 => WeatherCode.Cloudy,
+            < 51 => WeatherCode.Fog,
+            _ => WeatherCode.RainOrWorse
+        };
     }
 }
