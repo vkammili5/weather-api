e2f10fb2734599696b5e77fdafe5bbf93f495df5
namita1409 <81958036+namita1409@users.noreply.github.com>
2022-07-03 11:09:54 +0100

    register ICityService in Program.cs

--- WeatherAPI/Program.cs
+++ WeatherAPI/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IWeatherService, WeatherService>();
+builder.Services.AddScoped<ICityService, CityService>();
 
 builder.Services.AddControllers();
 
